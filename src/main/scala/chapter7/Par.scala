c0ec83cf66beea14c5ba403b8fa6b91d325204e3
Mauricio Linhares <mauricio.linhares@gmail.com>
2015-05-28 20:19:55 -0300

    Starting Parallel library implementation

---
+++ src/main/scala/chapter7/Par.scala
@@ -0,0 +1,14 @@
+package chapter7
+
+
+object Par {
+
+  def unit[A](a: A): Par[A] = ???
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
+  def fork[A](a: => Par[A]): Par[A]  = ???
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+  def run[A](a: Par[A]): A = ???
+
+}
+
+trait Par[T]
